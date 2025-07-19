7dafa49e77247a5a0ac25666ac4d9374579501a7
owolabi32 <owolabiidris230@gmail.com>
2025-07-19 19:25:30 +0100

    Refactor Main method to calculate and display letter grades based on user input

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,76 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        int grade = Convert.ToInt32(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign
+        if (letter != "F")
+        {
+            int lastDigit = grade % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+
+            // Handle exceptional cases
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Determine if the user passed the course
+        string passedMessage = grade >= 70 ? "Congratulations, you passed the course!" : "Don't worry, you'll do better next time!";
+
+        // Print the results
+        if (!string.IsNullOrEmpty(sign))
+        {
+            Console.WriteLine($"Your grade is {letter}{sign}.");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}.");
+        }
+        Console.WriteLine(passedMessage);
     }
 }
